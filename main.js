ac69c17dfaac1452541a04e4082f036d13efd985
Niharika655 <73527574+Niharika655@users.noreply.github.com>
2021-08-10 17:52:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.position(550,275);
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier("MobileNet", modelLoaded);
+}
+
+function modelLoaded() {
+  console.log("ModelLoaded");
+}
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+  classifier.classify(video, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    document.getElementById("resultObjectName").innerHTML = results[0].label;
+    document.getElementById("resultObjectAccuracy").innerHTML = results[0].confidence.toFixed(3);
+  }
+}
